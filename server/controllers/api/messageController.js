8bc4494e390865fb035944680f9e7947372dd6ed
William Taul <128314012+WilliamTaul@users.noreply.github.com>
2025-10-04 17:54:43 -0500

    darkend css style. beginning to add messsage

---
+++ server/controllers/api/messageController.js
@@ -0,0 +1,23 @@
+const Message = require('../../models/messageSchema');
+
+const getMessages = async (req, res) => {
+    // Development only return all messages
+    try {
+        const messages = await Message.find({});
+    } catch (err) {
+        console.error("Database error: ", err);
+        res.status(500).json({ error: "Failed to retrieve messages!" });
+    }
+};
+
+const newMessage = async (req, res) => {
+    try {
+        if (!req.body.content) return res.status(400).json({ error: "Message must contain content" });
+        const message = new Message({ content: req.body.content, senderId: req.body.senderId,
+                                    receiverId: req.body.receiverId });
+        await message.save();
+    } catch (err) {
+        console.error("Database error: ", err);
+        res.status(500).json({ error: "Server Error" });
+    }
+}
---
+++ server/models/messageSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    content: { type: String, required: true },
+    senderId: { type: String, required: true },
+    receiverId: { type: String, required: true },
+    createdAt: {type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('messageSchema');
+
