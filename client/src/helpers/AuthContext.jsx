1b7b1af597b61cf023a2f3800863a6b17f29e4f4
William Taul <128314012+WilliamTaul@users.noreply.github.com>
2025-10-07 14:56:24 -0500

    users now stay logged in if they have a valid refresh token on page refreshes

--- client/src/helpers/AuthContext.jsx
+++ client/src/helpers/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext, useEffect, useMemo , useRef} from 'react';
 import { jwtDecode } from 'jwt-decode';
 import axios from 'axios';
 
@@ -8,45 +8,81 @@ export function AuthProvider({ children }) {
     const [userId, setUserId] = useState(null);
     const [userRole, setUserRole] = useState(null);
     const [accessToken, setAccessToken] = useState(null);
-    const [isLoggedIn, setIsLoggedIn] = useState(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    
+    const refreshing = useRef(false);
 
-    const api = axios.create({
-    withCredentials: true,
+    const api = useMemo(() => {
+        const instance = axios.create({
+        withCredentials: true,
     });
 
-    api.interceptors.response.use(
+    instance.interceptors.response.use(
         response => response,
         async error => {
-            const originalRequest = error.config;
+        const originalRequest = error.config;
 
-            const excludedPaths = [
-                '/auth/login',
-                '/auth/register'
-            ]
+        const excludedPaths = [
+            '/auth/login',
+            '/auth/register',
+            '/auth/token'
+        ];
 
-            if (excludedPaths.some(path => originalRequest.url.includes(path))){
-                return Promise.reject(error);
-            }
-
-            if (error.response?.status === 401 && !originalRequest._retry && !originalRequest.url.includes('auth/token')) {
-                originalRequest._retry = true;
-
-                try {
-                    const res = await api.post('http://localhost:3001/auth/token');
-                    
-                    const newAccessToken = res.data.token;
-                    setAccessToken(newAccessToken)
-
-                    originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-                    return api(originalRequest);
-                } catch (err) {
-                    return Promise.reject(err);
-                }
-            }
-            
+        console.log("url:", originalRequest.url);
+        if (excludedPaths.some(path => originalRequest.url.includes(path))) {
+            console.log("excluded paths");
+            console.log("Promise error:", error);
             return Promise.reject(error);
         }
+
+        if (error.response?.status === 401 && !originalRequest._retry && !originalRequest.url.includes('auth/token')) {
+            originalRequest._retry = true;
+
+            try {
+            const res = await instance.post('http://localhost:3001/auth/token');
+
+            const newAccessToken = res.data.token;
+            setAccessToken(newAccessToken);
+
+            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+            return instance(originalRequest);
+            } catch (err) {
+            return Promise.reject(err);
+            }
+        }
+
+        return Promise.reject(error);
+        }
     );
+
+    return instance;
+    }, []);
+    console.log("effect mount")
+    useEffect(() => {
+        console.log("refreshing", refreshing)
+        if (refreshing.current) return;
+        refreshing.current = true;
+        const refreshPage = async () => {
+            try {
+                const res = await api.post('http://localhost:3001/auth/token');
+                const token = res.data.token;
+                if (token) {
+                    const decoded = jwtDecode(token);
+                    setAccessToken(token)
+                    setIsLoggedIn(true);
+                    setUserId(decoded.id);
+                    setUserRole(decoded.role);
+                }
+            } catch (err) {
+                 if (err.response && err.response.data && err.response.data.message) {
+                    console.error("Server Error Message:", err.response.data.message);
+                } else {
+                    console.error("Login Error:", err.message);
+                }
+            }
+        }
+        refreshPage();
+    }, []);
 
     const register = async (credentials) => {
         try {
@@ -106,8 +142,9 @@ export function AuthProvider({ children }) {
         setUserRole(null);
     };
 
+    const authValue = useMemo(() => ({accessToken, isLoggedIn, api, userId, userRole, register, login, logout}), [accessToken, isLoggedIn, userId, userRole]);
     return (
-        <AuthContext.Provider value={{ accessToken, isLoggedIn, api, userId, userRole, register, login, logout }}>
+        <AuthContext.Provider value={ authValue }>
             {children}
         </AuthContext.Provider>
     );
--- client/src/messages/Message.jsx
+++ client/src/messages/Message.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-import api from '../api';
-import "./styles.css";
+import "../styles.css";
 
 export function Message() {
     const [message, setMessage] = useState("");
